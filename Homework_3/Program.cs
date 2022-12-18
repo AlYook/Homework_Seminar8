5c4db084c61e0f9d4e023b1c8d042c3075afb4df
Aleksandra Obukh <sem0609@mail.ru>
2022-12-18 23:09:10 +0300

    fix mistake

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,15 +1,13 @@
 ﻿// Задайте две матрицы. Напишите программу, которая будет
 // находить произведение двух матриц.
 
-int[,] array1 = GetArrayMatrix(4, 5, 0, 10);
+int[,] array1 = GetArrayMatrix(4, 4, 1, 5);
 PrintArray(array1);
 Console.WriteLine();
-SumMartix(array1);
-int[,] array2 = GetArrayMatrix(4, 5, 0, 10);
+int[,] array2 = GetArrayMatrix(4, 4, 1, 5);
 PrintArray(array2);
 Console.WriteLine();
-SumMartix(array2);
-ProductMatrix(array1, array2);
+PrintArray(ProductMatrix(array1, array2, 4, 4));
 
 
 int[,] GetArrayMatrix(int rows, int columns, int minValue, int maxValue)
@@ -37,21 +35,18 @@ void PrintArray(int[,] inArray)
     }
 }
 
-int SumMartix(int[,] inArray)
+int[,] ProductMatrix(int[,] array1, int[,] array2, int rows, int columns)
 {
-    int sum = 0;
-    for (int i = 0; i < inArray.GetLength(0); i++)
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < array1.GetLength(0); i++)
     {
-        for (int j = 0; j < inArray.GetLength(1); j++)
+        for (int j = 0; j < array2.GetLength(1); j++)
         {
-            sum = sum + inArray[i,j];
-        }       
+            for (int k = 0; k < array2.GetLength(0); k++)
+            {
+                result[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
     }
-    return sum;
-}
-
-void ProductMatrix(int[,] array1, int[,] array2)
-{
-    int prod = SumMartix(array1) * SumMartix(array2);
-    Console.WriteLine($"произведение = {prod}");
+    return result;
 }
