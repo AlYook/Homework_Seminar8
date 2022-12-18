a539e12bbb88c4cf228d5d1a7d88317ccb637f0c
Aleksandra Obukh <sem0609@mail.ru>
2022-12-18 22:37:08 +0300

    solved task 5

---
+++ Homework_5/Program.cs
@@ -0,0 +1,60 @@
+﻿//Заполните спирально массив 4 на 4
+
+int[,] array = GetArrayMatrix(4);
+PrintArray(array);
+Console.WriteLine();
+
+
+
+int[,] GetArrayMatrix(int size)
+{
+    int n = 0;
+    int[,] helix = new int[size, size];
+    int j0 = 0;
+    int jn = size - 1;
+    int k0 = 0;
+    int kn = size - 1;
+    while (n < size * size)
+    {
+        for (int j = j0; j <= jn; j++)
+        {
+            helix[k0, j] = ++n;
+        }
+
+        k0++;
+
+        for (int k = k0; k <= kn; k++)
+        {
+            helix[k, jn] = ++n;
+        }
+
+        jn--;
+
+        for (int j = jn; j >= j0; j--)
+        {
+            helix[kn, j] = ++n;
+        }
+
+        kn--;
+
+        for (int k = kn; k >= k0; k--)
+        {
+            helix[k, j0] = ++n;
+        }
+
+        j0++;
+    }
+    return helix;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
